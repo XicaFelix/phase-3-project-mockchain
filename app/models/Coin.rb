c7b03618a342f978442803a20c7f71c6fe6d0066
Xica Felix <xicasfelix@gmail.com>
2022-12-30 22:37:35 -0500

    adding database relationships to models

--- app/models/Coin.rb
+++ app/models/Coin.rb
@@ -1,3 +1,3 @@
 class Coin < ActiveRecord::Base
-belongs_to :User
+belongs_to :user
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-
+    has_many :coins
+    has_many :transactions, :through :coins
 
 end
